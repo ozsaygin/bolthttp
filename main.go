2ae4d2e6ecbf9805675b93f39bd932004f66d80c
Oguz Ozsaygin <oguzozsaygin@sabanciuniv.edu>
2020-09-03 14:59:15 +0300

    Initialize server and log start message

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
+type Server struct {
+	address string
+	port    int
+}
+
+func (s * Server) Serve() {
+	log.Printf("Started to listen %s:%d", s.address, s.port)
+
+}
+
 func main() {
+	// Example
 	fmt.Println("Hello")
+	server := &Server{"127.0.0.1", 6789}
+	server.Serve()
+
 }
